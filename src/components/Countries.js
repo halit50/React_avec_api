ec1a86ec808b6412dd40fd14aa803c8d7e4d3c80
Halit <halit.cinici@live.fr>
2021-02-18 19:23:36 +0100

    Ajout de l'Api

---
+++ src/components/Countries.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Card from './Card';
+
+const Countries = () => {
+    // Hook
+    const [data,setData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag').then((res) => setData(res.data));
+    },[]);
+    
+    
+    return (
+        <div className='countries'>
+            <ul className='countries-list'>
+                {data.map((country) =>(
+                    <Card country={country} key={country.name} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Countries;
---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation/>
+            <Logo/>
+            <h1> A propos</h1>
+            <br/>
+            <p>
+            Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur. Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C. Fannius et Q. Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.
+            </p>
+            <br/>
+            <p>
+            Sed ut tum ad senem senex de senectute, sic hoc libro ad amicum amicissimus scripsi de amicitia. Tum est Cato locutus, quo erat nemo fere senior temporibus illis, nemo prudentior; nunc Laelius et sapiens (sic enim est habitus) et amicitiae gloria excellens de amicitia loquetur. Tu velim a me animum parumper avertas, Laelium loqui ipsum putes. C. Fannius et Q. Mucius ad socerum veniunt post mortem Africani; ab his sermo oritur, respondet Laelius, cuius tota disputatio est de amicitia, quam legens te ipse cognosces.
+            </p>
+        </div>
+    );
+};
+
+export default About;
